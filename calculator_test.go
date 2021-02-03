4cda782112f98d13144e570d021543b5bb1667d6
Jon Barber <jon.barber@acm.org>
2021-02-03 16:56:38 +0000

    Struct to hold test data and expected result

--- calculator_test.go
+++ calculator_test.go
@@ -2,15 +2,32 @@ package calculator_test
 
 import (
 	"calculator"
+	"fmt"
 	"testing"
 )
 
+type testcase struct {
+	a    float64
+	b    float64
+	want float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	tests := []testcase{
+		{a: 2, b: 2, want: 4},
+		{a: -1, b: 1, want: 0},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			got := calculator.Add(test.a, test.b)
+			if test.want != got {
+				t.Errorf("want %f, got %f", test.want, got)
+			}
+
+		})
 	}
 }
 
