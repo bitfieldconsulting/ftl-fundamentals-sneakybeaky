d80c6b1bbd214901e944a69d745d3639a4884de0
Jon Barber <jon.barber@acm.org>
2021-02-07 11:38:34 +0000

    Add a name field for each test case

--- calculator_test.go
+++ calculator_test.go
@@ -10,11 +10,13 @@ func TestAdd(t *testing.T) {
 	t.Parallel()
 
 	tcs := []struct {
+		name string
 		a, b float64
 		want float64
 	}{
-		{a: 2, b: 2, want: 4},
-		{a: -1, b: 1, want: 0},
+		{name: "Sum of 2 positives is a positive", a: 2, b: 2, want: 4},
+		{name: "A negative and a positive that sum to zero", a: -1, b: 1, want: 0},
+		{name: "A negative and a negative that sum to a negative", a: -1, b: -4, want: -5},
 	}
 
 	for i, test := range tcs {
